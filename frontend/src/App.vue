e3c8e92d02ebfedb8699d21b147c5d2fe1621d1f
Eric Gagné <egagne@cstj.qc.ca>
2021-03-28 13:52:17 -0400

    Frontend étape 1

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+@import url("https://rsms.me/inter/inter.css");
+html {
+  font-family: "Inter", sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
+#app #content {
+  display: flex;
+  height: 100vh;
+  background-color: rgb(46, 46, 46);
+  color: #fff;
+}
+</style>
---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,74 @@
+<!-- SECTION HTML -->
+<template>
+  <div id="content" class="login">
+    <main>
+      <form action="#" method="POST" @submit="login">
+        <input
+          type="text"
+          name="username"
+          class="username"
+          placeholder="Nom d'utilisateur"
+          v-model="username"
+          required
+        />
+        <input type="submit" value="Se connecter" class="submit" />
+      </form>
+    </main>
+  </div>
+</template>
+
+<!-- SECTION CODE -->
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+    };
+  }, // FIN DATA
+  methods: {
+    login(e) {
+      e.preventDefault();
+      //console.log(this.username);
+      this.$router.push({
+        path: "/chat/" + this.username,
+      });
+    },
+  },
+};
+</script>
+
+<!-- SECTION CSS -->
+<style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.login input.nom {
+  width: 300px;
+  border-radius: 8px;
+  border: none;
+  padding: 15px 15px;
+  background: #464646;
+  font-size: 16px;
+  color: #ccc;
+}
+
+.login input.submit {
+  border-radius: 8px;
+  border: none;
+  padding: 15px 15px;
+  margin-left: 5px;
+  background-color: #0f5781;
+  font-size: 16px;
+  color: #ccc;
+  transition: background-color 0.2s ease;
+  cursor: pointer;
+}
+.login input.submit:hover {
+  background-color: #0c4769;
+}
+</style>
